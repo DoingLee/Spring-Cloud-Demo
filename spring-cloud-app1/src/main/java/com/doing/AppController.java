e15c3a9032b2643310a85775ad99e2cd8548fb83
lidongying <lidongying@corp.netease.com>
2018-02-10 10:57:43 +0800

    @dev hystrix 断路器；统一spring cloud版本依赖

--- spring-cloud-app1/src/main/java/com/doing/AppController.java
+++ spring-cloud-app1/src/main/java/com/doing/AppController.java
@@ -1,5 +1,6 @@
 package com.doing;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,9 +16,14 @@ public class AppController {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "helloFallback") //该注解可以在任意方法上（不一定是controller里的方法）; 默认超时时间是2000毫秒; ﻿默认相同groupkey(默认是classname)的命令使用同一个线程池﻿threadPoolKey
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         System.out.println("Hello ! I am app1.");
-        return "Hello ! I am app1. \n " + restTemplate.getForEntity("http://SERVICE1/hello", String.class).getBody();
+        return "Hello ! I am app1. \n " + restTemplate.getForEntity("http://SERVICE1/hello", String.class).getBody(); //使用应用名SERVICE1，而非ip（客户端负载均衡）
+    }
+
+    public String helloFallback() {
+        return "sorry ! app1 CircuitBreaker fallback";
     }
 }
--- spring-cloud-app1/src/main/java/com/doing/Application.java
+++ spring-cloud-app1/src/main/java/com/doing/Application.java
@@ -2,6 +2,7 @@ package com.doing;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableDiscoveryClient //eureka client
+@EnableCircuitBreaker //hystrix
+//@SpringCloudApplication //包含注解：@SpringBootApplication、@EnableDiscoveryClient、@EnableCircuitBreaker
 public class Application {
 
     public static void main(String[] args) {
